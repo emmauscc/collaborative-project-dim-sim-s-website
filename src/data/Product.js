8286e2dc5b113fa3c375cd3b8b52f286d221d780
bevan654 <bevan.shajan654@gmail.com>
2022-09-20 14:55:10 +0930

    product data structure

---
+++ src/data/Product.js
@@ -0,0 +1,17 @@
+
+/**
+ * Class representing a product
+ */
+
+
+class Product{
+    /**
+     * 
+     * @param {String} id // unique identifier for product. eg. 123ASKDIZ2
+     * @param {String} name // product name. eg. Red Heating Socks
+     */
+    constructor(id,name){
+        this.id = id
+        this.name = name
+    }
+}
