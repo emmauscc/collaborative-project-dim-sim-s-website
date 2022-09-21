e97b1c85525752049ae101574cca10bdb1c4671f
slavish_luke <luke.kradolfer@gmail.com>
2022-09-21 21:09:47 +0930

    made login system & connected pages

---
+++ src/login.js
@@ -0,0 +1,15 @@
+function checkDetails(){
+    
+    $("#wrong").empty();
+
+    if($("#username").val() == "admin" && $("#password").val() == "password"){
+        window.location.href = "admin.html"
+    }else{
+        
+        $("#wrong").append("<h2>Invalid Credentials</h2>")
+        document.querySelector("#wrong").style.animation = 'none';
+        document.querySelector("#wrong").offsetWidth;
+        document.querySelector("#wrong").style.animation = '1.2s shake';
+    }
+
+}
