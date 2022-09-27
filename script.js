a2bd3bf3c954f569ae295bd5082d6da183a206ac
bevan654 <bevan.shajan654@gmail.com>
2022-09-27 13:57:53 +0930

    firebase handler

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+
+console.log(new FireBaseHandler().get_specific_product('69420'))
--- src/data/Product.js
+++ src/data/Product.js
@@ -10,8 +10,16 @@ class Product{
      * @param {String} id // unique identifier for product. eg. 123ASKDIZ2
      * @param {String} name // product name. eg. Red Heating Socks
      */
-    constructor(id,name){
+    constructor(id,name,image,keywords,price,num_available,available_sizes){
         this.id = id
         this.name = name
+        this.image = image
+        this.keywords = keywords.split(',')
+        this.price = price
+        this.published_date = published_date
+        this.num_available = num_available
+        this.available_sizes = available_sizes.split(',')
+
     }
 }
+
