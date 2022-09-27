6f9fc87b5e3653e02f0d32b1ea8c26b6ff20a92e
bevan654 <bevan.shajan654@gmail.com>
2022-09-27 14:04:07 +0930

    script.js changes

--- script.js
+++ script.js
@@ -5,4 +5,27 @@
 
 
 
-console.log(new FireBaseHandler().get_specific_product('69420'))
+class Structure{
+    constructor(){
+        this.firebase_handler = new FireBaseHandler()
+
+
+
+
+
+
+        this.test()
+    }
+
+
+
+    test = async () => {
+        console.log(await this.firebase_handler.get_specific_product('69420'))
+        
+    }
+}
+
+
+
+
+new Structure()
