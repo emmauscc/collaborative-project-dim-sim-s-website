c90970e25e9ac70bfe91d720d0b99ed712502868
slavish_luke <luke.kradolfer@gmail.com>
2022-10-20 14:41:55 +1030

    fixed mistake

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add_product");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 var items = [new Product("0001","Test Product",null,"test, product",69.99)]
 
 
