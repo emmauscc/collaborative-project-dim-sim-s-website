c722d10a0e461b4bf9a25fe7c34a47ce5f9e3d06
slavish_luke <luke.kradolfer@gmail.com>
2022-10-20 19:13:14 +1030

    attempting modal boxes

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+let product;
+let currentID = "";
+let clickedID = "";
+let date = "";
+let newdate = "";
+
+
+const firebaseConfig2 = {
+  apiKey: "AIzaSyD8THmhiu-yyHXJZVy2BbsL1Vvep7cqds8",
+  authDomain: "dim-sim-s-website.firebaseapp.com",
+  databaseURL: "https://dim-sim-s-website-default-rtdb.firebaseio.com",
+  projectId: "dim-sim-s-website",
+  storageBucket: "dim-sim-s-website.appspot.com",
+  messagingSenderId: "1079766995401",
+  appId: "1:1079766995401:web:96f174fc09f1a09b2f9818"
+
+};
+
+firebase.initializeApp(firebaseConfig2);
+
+function readAllData(){
+  var getUserId = firebase.database().ref();
+
+  getUserId.on('value', (snapshot) => {
+    const data = snapshot.val();
+
+    console.log(data);
+
+    products = data['data']['products']
+    console.log(products)
+    
+    for(var i=0; i<Object.keys(products).length; i++){
+
+      currentID = products[Object.keys(products)[i]]['id']
+      console.log(currentID)
+
+      let date = JSON.stringify(new Date(products[currentID]['release_date']))
+      newdate = date.substr(0, date.indexOf("T")).slice(1)
+      $("#productContainer").append(`
+        
+        <div class="product" onclick="generateModal(this.id)" id="${currentID}">
+          <img class="productImg" src="${products[currentID]['image']}">
+          <div class="productName">${products[currentID]['name']}</div>
+          <div class="productBrand">${products[currentID]['brand']}</div>
+          <div class="productPrice">$${products[currentID]['price']}</div>
+        </div>
+
+        
+      `)
+    }
+  
+  })
+}
+readAllData();
+
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -13,36 +69,47 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("add_product");
+function generateModal(clickedID){
+  modal.style.display = "block"
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  console.log(clickedID)
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+  console.log(products)
+
+  $("body").append(`
+    <div id="Modal${clickedID}" class="modal">
+      <div class="modal-content">
+        <span class="close"></span>
+
+        <div class="productInformation">
+          <img class="Image" src="${products[clickedID]['image']}">
+          <div class="leftContainer">
+            <div class="Name">${products[clickedID]['name']} -</div>
+            <div class="Brand">${products[clickedID]['brand']}</div>
+            <div class="ReleaseDate">${newdate}</div>
+          </div>
+
+          <div class="rightContainer">
+            <div class="Available"><b>${products[clickedID]['num_available']}</b> More in stock!</div>
+            <div class="Price">${products[clickedID]['price']}</div>
+          </div>
+  `)
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById("Modal"+clickedID);
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-var items = [new Product("0001","Test Product",null,"test, product",69.99)]
+// var items = [new Product("0001","Test Product",null,"test, product",69.99)]
 
 
-items[0].variants.push(new Variant())
+// items[0].variants.push(new Variant())
 
 
 
